52602809a531aaf9cefa218bc6cb00d7f83a4950
Sam Gaus <sam@gaus.co.uk>
2025-12-04 14:24:16 +0000

    Do not transact thousands of row changes

--- packages/excavator-web/prisma/migrations/20251204133730_skeletons/data-migration.ts
+++ packages/excavator-web/prisma/migrations/20251204133730_skeletons/data-migration.ts
@@ -5,26 +5,24 @@ import { hashData } from "../../../etl.js";
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.$transaction(async (tx) => {
-    const data = await tx.spadingData.findMany({
-      where: {
-        project: "Skeleton of Crimbo Past",
+  const data = await prisma.spadingData.findMany({
+    where: {
+      project: "Skeleton of Crimbo Past",
+    },
+  });
+
+  for (const datum of data) {
+    await prisma.spadingData.update({
+      where: { id: datum.id },
+      data: {
+        dataHash: hashData(
+          datum.data as unknown as Record<string, string | number | boolean>,
+        ),
       },
     });
+  }
 
-    for (const datum of data) {
-      await tx.spadingData.update({
-        where: { id: datum.id },
-        data: {
-          dataHash: hashData(
-            datum.data as unknown as Record<string, string | number | boolean>,
-          ),
-        },
-      });
-    }
-
-    console.log(data.length, "rows updated");
-  });
+  console.log(data.length, "rows updated");
 }
 
 main()
