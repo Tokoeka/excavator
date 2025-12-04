041eb1e452f8228568ee84ad61b9fe960576023c
Sam Gaus <sam@gaus.co.uk>
2025-12-04 17:13:34 +0000

    Handle updates

--- packages/excavator-web/prisma/migrations/20251204133730_skeletons/data-migration.ts
+++ packages/excavator-web/prisma/migrations/20251204133730_skeletons/data-migration.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 import { hashData } from "../../../etl.js";
 
@@ -12,14 +13,44 @@ async function main() {
   });
 
   for (const datum of data) {
-    await prisma.spadingData.update({
-      where: { id: datum.id },
-      data: {
-        dataHash: hashData(
-          datum.data as unknown as Record<string, string | number | boolean>,
-        ),
-      },
-    });
+    const dataHash = hashData(
+      datum.data as unknown as Record<string, string | number | boolean>,
+    );
+    try {
+      await prisma.spadingData.update({
+        where: { id: datum.id },
+        data: {
+          dataHash,
+        },
+      });
+    } catch (error) {
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === "P2002"
+      ) {
+        const existing = await prisma.spadingData.findFirst({
+          where: { dataHash, version: datum.version, project: datum.project },
+        });
+
+        if (existing) {
+          await prisma.report.updateMany({
+            where: { dataId: datum.id },
+            data: { dataId: existing.id },
+          });
+        }
+
+        await prisma.spadingData.delete({
+          where: { id: datum.id },
+        });
+
+        console.log(
+          "Discarded duplicate row with id",
+          datum.id,
+          "as it was the same as",
+          existing?.id ?? "no other row??",
+        );
+      }
+    }
   }
 
   console.log(data.length, "rows updated");
