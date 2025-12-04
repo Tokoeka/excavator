8dfc80c7845eb8b5678a24cc846e90218d544262
Sam Gaus <sam@gaus.co.uk>
2025-12-04 13:44:52 +0000

    Add migration for old skele data

---
+++ packages/excavator-web/prisma/migrations/20251204133730_skeletons/data-migration.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from "@prisma/client";
+
+import { hashData } from "../../../etl.js";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.$transaction(async (tx) => {
+    const data = await tx.spadingData.findMany({
+      where: {
+        project: "Skeleton of Crimbo Past",
+      },
+    });
+
+    for (const datum of data) {
+      if (datum.data.skeleton !== undefined) continue;
+
+      await tx.spadingData.update({
+        where: { id: datum.id },
+        data: {
+          dataHash: hashData(
+            datum.data as unknown as Record<string, string | number | boolean>,
+          ),
+        },
+      });
+    }
+
+    console.log(data.length, "rows updated");
+  });
+}
+
+main()
+  .catch(async (e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => await prisma.$disconnect());
